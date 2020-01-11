28e373d2556e94ac9bfea536d53cf33256f881e3
Marta Ramos <martavmr26297@gmail.com>
2020-01-11 00:52:36 +0000

    Created unstyled index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'articles#index'
-  resources :articles
+  resources :articles, only: [:index, :show]
+
+  namespace :admin do
+    resources :articles
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
