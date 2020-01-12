cbb1ab6b366b6c7493bb36f343bd7c3a6471bc2d
Marta Ramos <martavmr26297@gmail.com>
2020-01-12 22:15:24 +0000

    Created index and show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,13 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show]
   def index
     @articles = policy_scope(Article)
+  end
+
+  def show
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
