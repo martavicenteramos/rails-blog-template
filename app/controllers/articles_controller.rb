7ed0ca3e5113d5003ba4f8a27b98556ac8136bf2
Marta Ramos <martavmr26297@gmail.com>
2020-01-14 23:28:34 +0000

    Created homepage

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show]
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
-    @articles = policy_scope(Article)
+    @articles = policy_scope(Article).order(created_at: :desc).limit(3)
   end
 
   def show
--- app/policies/article_policy.rb
+++ app/policies/article_policy.rb
@@ -8,4 +8,8 @@ class ArticlePolicy < ApplicationPolicy
   def index?
     true
   end
+
+  def show?
+    true
+  end
 end
